daf4521a18cca0b38cb9e32ebd81adaa70a7e3ba
David Wenzel <dwenzel95@gmail.com>
2022-04-02 10:04:50 -0500

    commit tree bae525ff3a93bf37b5bcd6fc9959363914e46ee1

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
+errorModal.classList.add('hidden')
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('dom loaded')
+  
+  
+clickListener()
+findLikes()
+})
+function hideError(){
+  errorModal.classList.add('hidden')
+}
+function findLikes(){
 
+ const likeArr= document.querySelectorAll(".like-glyph")
+
+ likeArr.forEach((singularLike) => {
+   singularLike.addEventListener('click', () => console.log('like-glyph class clicked'))
+ })
+}
+function clickListener() {
+  document.addEventListener('click', (e) => {
+    if(e.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+      .then(res => {
+         const activated = e.target.classList.contains('activated-heart')
+         if(activated){
+           e.target.classList.remove('activated-heart')
+           e.target.innerHTML = EMPTY_HEART
+         }else{
+           e.target.classList.add('activated-heart')
+           e.target.innerHTML = FULL_HEART
+         }
+         
+      })
+      .catch(error => errorModal.classList.remove('hidden'))
+      setTimeout(hideError, 3000)
+    }
+
+  })
+}
 
 
 
